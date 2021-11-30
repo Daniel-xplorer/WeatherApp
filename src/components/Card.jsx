ebb829d5d78ff9ae129d2f9d0cd27cc7976aeef3
Daniel Amado <84291098+Daniel-xplorer@users.noreply.github.com>
2021-11-30 18:17:00 -0500

    module card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div >
-    ñlsneflkej
+  return <div className="card">
+    <div className="cardHead">
+      <button onClick={props.onClose} className="close">x</button>
+      <h1 className="cityName">{props.name}</h1>
+    </div>
+      <h4>min</h4>
+      <span> {props.min}</span>
+    <div>
+    </div>
+      <h4>MAX</h4>
+      <span> {props.max}</span>
+    <div>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="imagen del clima"></img>
+    </div>
   </div>
 };
-
