a8c04ef0233fa91c77aa6b1b570cf4f012a6ed2f
Daniel Amado <84291098+Daniel-xplorer@users.noreply.github.com>
2021-11-30 19:43:49 -0500

    module minCard for card

---
+++ src/components/minCard.jsx
@@ -0,0 +1,9 @@
+//codigo reutilizable para la parte de max y min en la card
+
+export default function MinCard (props){
+    return(
+    <div>
+        <h4>{props.state}</h4>
+        <span>{props.valuestate}</span>
+    </div>
+)};
